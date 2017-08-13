b5ed25e210f0020da520b25f1b4e5fa26d2b1c78
dariasteam <dariasteam94@gmail.com>
2017-08-13 11:21:36 +0100

    Add tree parts to tileset

--- Scenes/Events/enemy_1.gd
+++ Scenes/Events/enemy_1.gd
@@ -25,12 +25,12 @@ func reverse_direction():
 func _ready():
 	if (!dir_left):
 		reverse_direction()
-	set_process(true)
+	set_fixed_process(true)
 	#set_process(true)
 
 func dissapear():
 	sprite.set_opacity(0)
-	set_process(false)
+	set_fixed_process(false)
 	set_layer_mask_bit(2,false)
 	
 	hit_single.set_emitting(true)
@@ -91,7 +91,7 @@ func change_velocity(amount, right):
 		v.x += ((dir_left * -1) + (!dir_left * 1)) * amount
 	
 
-func _process(delta):
+func _fixed_process(delta):
 	var motion = v * delta
 	motion = move(motion)
 	v.y += delta * GRAVITY
