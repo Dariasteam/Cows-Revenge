2c2daccb71408284aafe48d269e9437561e51c46
dariasteam <dariasteam94@gmail.com>
2017-10-02 19:25:06 +0100

    Add 'Cave Story' font

--- Scenes/Events/death_line.gd
+++ Scenes/Events/death_line.gd
@@ -3,6 +3,5 @@ extends Area2D
 func _on_DeathLine_body_enter( body ):
 	if(body.is_in_group("player")):
 		body.on_receive_damage(1000)
-		queue_free()
 	elif(body.is_in_group("enemy")):
 		body.queue_free()
