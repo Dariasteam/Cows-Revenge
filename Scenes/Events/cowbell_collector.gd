e5f54c717542f6071e1ed3fc55276e9e806fac64
dariasteam <dariasteam94@gmail.com>
2017-10-11 14:19:23 +0100

    Improve cowbells sound system

--- Scenes/Events/cowbell_collector.gd
+++ Scenes/Events/cowbell_collector.gd
@@ -5,6 +5,7 @@ extends Node2D
 # var b = "textvar"
 
 onready var timer = get_node("next_cowbell_time")
+const SAMPLE_LIBRARY = preload("res://Scenes/Events/cowbell_sample_library.tres")
 
 export(float) var pitch_increment = 0.05
 export(float) var base_pitch = 1.5
@@ -16,8 +17,25 @@ func timer_timeout():
 
 func play_sound():
 	pitch += pitch_increment
+	
+	var sample_player = SamplePlayer.new()
+	sample_player.set_sample_library(SAMPLE_LIBRARY)
+	sample_player.set_default_pitch_scale(pitch)	
+	sample_player.set_default_volume(40)
+	
+	var aux_timer = Timer.new()
+	aux_timer.set_one_shot(true)
+	aux_timer.set_wait_time(0.5)
+	
+	aux_timer.connect("timeout",sample_player,"queue_free")
+	
+	add_child(sample_player)
+	sample_player.add_child(aux_timer)	
+	
+	sample_player.play("cowbell_sound")
+	aux_timer.start()
+	
 	timer.start()
-	return pitch
 
 func _ready():	
 	timer.connect("timeout",self,"timer_timeout")
