2c395a3c463ac963a65ea7d215763a6fe7056369
dariasteam <dariasteam94@gmail.com>
2018-02-24 22:39:33 +0000

    Fix problem with game over screen

--- Scenes/Menu/game_over.gd
+++ Scenes/Menu/game_over.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
+func _ready():	
 	pass
 
 func _on_reset_pressed():
 	global.release_all()
 	get_tree().change_scene("res://Scenes/Menu/HUD.tscn")
+	queue_free()
 
 func _on_main_menu_pressed():	
 	get_tree().change_scene("res://Scenes/Menu/main_menu.tscn")
+	queue_free()
