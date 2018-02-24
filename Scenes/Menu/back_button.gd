5ba5590f1bf1847688e099b5a850547a42dcff6a
dariasteam <dariasteam94@gmail.com>
2018-02-24 13:09:05 +0000

    Fix pause problems with player inputs

--- Scenes/Menu/back_button.gd
+++ Scenes/Menu/back_button.gd
@@ -3,8 +3,13 @@ extends Button
 export(PackedScene) var scene
 
 func _ready():
-	connect("pressed", self, "_on_Button_pressed")
+	connect("pressed", self, "_on_Button_pressed")	
+	set_process_input(true)
 
 func _on_Button_pressed():		
 	get_tree().set_pause(false)
 	get_tree().change_scene_to(scene)
+
+func _input(ev):	
+	if (ev.is_action_pressed("ui_cancel")):
+		_on_Button_pressed()
--- Scenes/Menu/resume_game.gd
+++ Scenes/Menu/resume_game.gd
@@ -2,4 +2,11 @@ extends Button
 
 func _on_Resume_pressed():
 	get_tree().set_pause(false)
-	get_parent().set_hidden(true)
+	get_parent().set_hidden(true)	
+
+func _ready():
+	set_process_input(true)
+
+func _input(ev):
+	if (ev.is_action_pressed("ui_cancel")):
+		_on_Resume_pressed()		
