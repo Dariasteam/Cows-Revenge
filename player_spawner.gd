bcbe06000274c219940ce544590e00c7e625aff1
dariasteam <dariasteam94@gmail.com>
2018-02-25 19:40:32 +0000

    Starting localization, menus and tutorial complete

--- player_spawner.gd
+++ player_spawner.gd
@@ -6,20 +6,16 @@ onready var sound = get_node("Sound")
 onready var instancer = get_node("Instancer")
 onready var texts = get_node("UFO_texts")
 onready var total_cages = get_tree().get_nodes_in_group("cages").size()
-
-export(String, MULTILINE) var init_text = "Vuelve cuando hayas\n salvado a todas las gallinas."
-export(String, MULTILINE) var append_text
-
-var player
-
 onready var cow_is_abducted = false
 
+var init_text = tr("UFO_JAIL_COUNTER")
+
+var player
 var mission_acomplished = false
 var rest = total_cages
 
-
 func _ready():	
-	texts.set_text(str(init_text, "\n", append_text, total_cages, "."))
+	texts.set_text(str(init_text, total_cages, "."))
 	sound.play()
 	anim.play("appear")
 	player = PLAYER.instance()
@@ -48,15 +44,14 @@ func can_liftoff():
 		
 func update_text():
 	rest = total_cages - player.cages_open
-	texts.set_text(str(init_text, "\n", append_text, rest, "."))
+	texts.set_text(str(init_text, rest, "."))
 
 func _input(ev):
 	if (ev.is_action_pressed("ui_up")):
 		abduct()
 	
 func _on_Area2D_body_enter( body ):
-	if (body.is_in_group("player")):
-		update_text()
+	if (body.is_in_group("player")):		
 		set_process_input(true)
 	
 func _on_Area2D_body_exit(body):
