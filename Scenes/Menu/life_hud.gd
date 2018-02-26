6c88cd254453edc0d1d5fc0133cb759f68a792ec
dariasteam <dariasteam94@gmail.com>
2018-02-26 10:27:05 +0000

    Fix minor bugs

--- Scenes/Menu/life_hud.gd
+++ Scenes/Menu/life_hud.gd
@@ -7,10 +7,12 @@ export var heart_inter_distance = 64
 export var initial_heart_pos = Vector2(32, 0)
 
 func reset_values():
-	on_set_max_life(global.max_life)
-	on_update_life(global.life)
+	on_set_max_life(global.max_life)	
 
 func on_set_max_life(value):
+	for element in get_children():
+		element.free()
+	
 	n_hearts = value
 	var next_heart_pos = initial_heart_pos
 	for i in range(0, n_hearts):
@@ -18,14 +20,16 @@ func on_set_max_life(value):
 		next_heart.set_pos(next_heart_pos)
 		add_child(next_heart)
 		next_heart_pos.x += heart_inter_distance
+	
+	on_update_life(global.max_life)
 
 func on_update_life (var n):
 	if (n <= 0):
 		get_tree().change_scene("res://Scenes/Menu/game_over.tscn")
 	else:
-		for i in range(0,n):
+		for i in range(0, n):			
 			get_children()[i].show()
-		for i in range(n, n_hearts):
+		for i in range(n, global.max_life):			
 			get_children()[i].hide()
 	
 	
