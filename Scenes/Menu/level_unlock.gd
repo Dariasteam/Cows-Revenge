d67e50f16145d71c77bc241e9858200974d853f8
dariasteam <dariasteam94@gmail.com>
2018-02-26 12:48:30 +0000

    Improve gamepad support

--- Scenes/Menu/level_unlock.gd
+++ Scenes/Menu/level_unlock.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 func _ready():
-	var levels = get_node("ScrollContainer/VBoxContainer").get_children()
+	var levels = get_node("VButtonArray/ScrollContainer/VBoxContainer").get_children()
 	for i in range (0, min(global.unlocked_levels, levels.size())): 
 		levels[i].set_disabled(false)
 	
-	get_node("ScrollContainer").grab_focus()
-	get_node("ScrollContainer/VBoxContainer").grab_focus()
-	get_node("ScrollContainer/VBoxContainer/Button1").grab_focus()
+	get_node("VButtonArray/ScrollContainer").grab_focus()
+	get_node("VButtonArray/ScrollContainer/VBoxContainer").grab_focus()
+	get_node("VButtonArray/ScrollContainer/VBoxContainer/Button1").grab_focus()
