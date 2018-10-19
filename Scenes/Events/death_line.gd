5bbc7c4738a710a125bf2a94b2571aeffe0c7d7a
dariasteam <dariasteam94@gmail.com>
2018-10-19 06:18:01 +0100

    Add botom level particles

--- Scenes/Events/death_line.gd
+++ Scenes/Events/death_line.gd
@@ -1,7 +1,17 @@
 extends Area2D
 
+onready var particles = get_tree().get_nodes_in_group("bottom_level_particles")[0]
 func _on_DeathLine_body_enter( body ):
 	if(body.is_in_group("player")):
 		body.on_receive_damage(1000)
 	elif(body.is_in_group("enemy")):
-		body.queue_free()
+		body.queue_free()	
+
+
+func _on_death_line_area_enter( area ):
+	if(area.is_in_group("bottom_level_detector")):		
+		particles.set_emitting(true)		
+
+func _on_death_line_area_exit( area ):
+	if(area.is_in_group("bottom_level_detector")):
+		particles.set_emitting(false)
